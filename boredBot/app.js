d3a4f539aa3757ec84ae3f07019400bfeb0a737f
LONEWOLF143 <38281542+LONEWOLF143@users.noreply.github.com>
2022-07-18 20:25:32 +0300

    Update app.js

--- boredBot/app.js
+++ boredBot/app.js
@@ -1,5 +1,5 @@
 document.getElementById("get-activity").addEventListener("click", function() {
-    fetch("https://apis.scrimba.com/bored/api/activity")
+    fetch('url')
       .then(response => response.json())
       .then(data => {
         document.getElementById("activity").textContent = data.activity
